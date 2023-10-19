dd2fd94c9df0c7c104206764956e7c496fb41858
Francisco Larrosa <f.larrosa@sieteideas.com.ar>
2023-10-19 16:17:33 -0300

    MAny changes

--- src/app/components/champion/champion.component.ts
+++ src/app/components/champion/champion.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Component, OnInit, OnDestroy, inject } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Subject, takeUntil } from 'rxjs';
 import { champAnimation } from 'src/app/core/animation';
@@ -9,16 +9,23 @@ import { ChampionService } from 'src/app/services/lol-service.service';
   selector: 'app-champion',
   templateUrl: './champion.component.html',
   styleUrls: ['./champion.component.scss'],
-  animations:[champAnimation]
+  animations: [champAnimation]
 })
 export class ChampionComponent implements OnInit, OnDestroy {
   nameChamp!: string;
-  tittleChamp!:string;
-  imgChamp!:string;
+  tittleChamp!: string;
+  imgChamp!: string;
+  skinArray: string[] = [];
+  champion!: ChampionData;
+  championLore!: string;
+  championTags!: any;
+  championDifficult!: any;
+  numDifficult!: any;
+  private _champService = inject(ChampionService);
   private _unsubscribeAll: Subject<any>;
 
 
-  constructor(private _champService: ChampionService, private route: ActivatedRoute) {
+  constructor(private route: ActivatedRoute) {
     this._unsubscribeAll = new Subject();
   }
 
@@ -26,6 +33,17 @@ export class ChampionComponent implements OnInit, OnDestroy {
   ngOnInit() {
     this.getChampData().then(champion => {
       this.loadData(champion);
+      this.loadSkins(this.nameChamp);
+      this.championLore = this.champion.data[this.nameChamp].lore;
+      this.championTags = this.champion.data[this.nameChamp].tags[0];
+      this.numDifficult = this.champion.data[this.nameChamp].info.difficulty;
+        if((this.numDifficult >= 7)){
+          this.championDifficult = 'High';
+        }else if ((this.numDifficult < 7 && this.numDifficult >=4)){
+          this.championDifficult = 'Medium';
+        }else{
+          this.championDifficult = 'Low';
+        }
     });
   }
 
@@ -47,28 +65,37 @@ export class ChampionComponent implements OnInit, OnDestroy {
 
     return new Promise<ChampionData>((resolve, reject) => {
       this._champService.getChampionData(this.nameChamp)
-      .pipe(takeUntil(this._unsubscribeAll))
-      .subscribe(
-        {
-          next: (champion: ChampionData) => {
-            resolve(champion);
-          },
-          error: (error: any) => {
-            reject(error);
+        .pipe(takeUntil(this._unsubscribeAll))
+        .subscribe(
+          {
+            next: (champion: ChampionData) => {
+              this.champion = champion;
+              resolve(champion);
+            },
+            error: (error: any) => {
+              reject(error);
+            }
           }
-        }
-      );
+        );
     }
     );
   }
 
-  loadData(champ:ChampionData){
+  loadData(champ: ChampionData) {
     this.tittleChamp = champ.data[this.nameChamp].title;
     //Load champ image default
-    this.imgChamp =  this.getSkinImage(this.nameChamp,0);
+    this.imgChamp = this.getSkinImage(this.nameChamp, 0);
   }
 
-  getSkinImage(name:string,num: number) {
+  getSkinImage(name: string, num: number) {
     return `https://ddragon.leagueoflegends.com/cdn/img/champion/splash/${name}_${num}.jpg`;
+  }
+
+  loadSkins(nameChamp: any) {
+    for (let index = 0; index < this.champion.data[nameChamp].skins.length; index++) {
+      this.skinArray.push(`https://ddragon.leagueoflegends.com/cdn/img/champion/splash/${nameChamp}_${this.champion.data[nameChamp].skins[index].num}.jpg`)
+    }
+    // I delete the first element why it is the default skin
+    this.skinArray.shift();
   }
 }
